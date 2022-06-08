c00291f7530c12de8a7e9a2c7270de12bc5eb9b1
JQUAN CALEK <jquancollins96@gmail.com>
2022-06-08 14:59:22 -0400

    wip: new react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from  "react";
+import logo from "./logo.svg";
+import "./App.css";
+// WE IMPORT OUR COMPONENTS
+import MovieDisplay from "./components/MovieDisplay";
+import Form from "./components/Form";
 
 function App() {
+  //variable with your apiKey
+  const apiKey = "98e3fb1f";
+
+  //State to hold movie data
+  const [movie, setMovie] = React.useState(null);
+
+  //Function to getMovies
+  const getMovie = async (searchTerm) => {
+    // make fetch request and store response
+    const response = await fetch(
+      `http://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`
+    );
+    // Parse JSON response into a javascript object
+    const data = await response.json();
+    //set the Movie state to the movie
+    setMovie(data);
+  };
+
+  //This will run on the first render but not on subsquent renders
+  React.useEffect(() => {
+    getMovie("Clueless");
+  }, []);
+
+  // USE OUR COMPONENTS IN APPs RETURNED JSX
+  // We pass the getMovie function as a prop called moviesearch
+  // We pass movie as props to movie display
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form moviesearch={getMovie} />
+      <MovieDisplay movie={movie} />
     </div>
   );
 }
---
+++ src/components/Form.js
@@ -0,0 +1,41 @@
+
+import React from "react";
+
+// Define a function that is our component, always make sure to declare the props parameter so you can use props in your component
+const Form = (props) => {
+  //State to hold the data of our form
+  const [formData, setFormData] = React.useState({
+    searchterm: "",
+  });
+
+  //handleChange - updates formData when we type into form
+  const handleChange = (event) => {
+    //use the event object to detect key and value to update
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = (event) => {
+    //prevent page from refreshing on form submission
+    event.preventDefault();
+    //pass the search term to moviesearch prop, which is apps getMovie function
+    props.moviesearch(formData.searchterm);
+  };
+
+  //The component must return some JSX
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="searchterm"
+          onChange={handleChange}
+          value={formData.searchterm}
+        />
+        <input type="submit" value="submit" />
+      </form>
+    </div>
+  );
+};
+
+// We must export the component to use it in other files
+export default Form;
